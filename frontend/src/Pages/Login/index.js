4e26612d919b9224755d52881ccce6e0c41cc195
daniya-PRO <syedadaniyahaider@gmail.com>
2022-09-28 10:04:47 +0500

    work

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,51 +1,51 @@
 import React from "react";
-import './index.css'
-const LoginPage = ()=>{
-return(
-<div className="center"> 
-
-<div className="flex justify-center items-center min-h-[90vh]">
-<form className="">
- <span className="border-[3px] border-solid border-black text-center inline-block">
-       <h1 className="text-2xl">Welcome Back</h1>
- <label><b>Username</b></label>
-    <input type="text" className="border-2 border-gray-200" placeholder="Enter Username" required/><br/> 
-
-    <label><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" required/><br/>
-        <br/>
-    <button type="submit">Login</button>
-    <label><br/>
-      <input type="checkbox" checked="checked"/> Remember me
-    </label>    <span>Forgot <a href="#">password?</a></span>
-
-  </span>
-</form>
-</div>
-</div>
-)
-
-
-
-}
-export default LoginPage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import "./index.css";
+const LoginPage = () => {
+  return (
+    <div className="center font-mono">
+      <div className="flex justify-center items-center min-h-[90vh]">
+        <form className="">
+          <span className="border-[3px] border-solid border-black text-center inline-block">
+            <br/> <h1 className="text-2xl">Login</h1>
+            <br/>
+            <div className="text-left padding-l-20x  padding-r-20x">
+              <label>Username:</label>
+              <br/>
+              <input
+                type="text"
+                className="border-b-2 border-gray-200 w-[100%]"
+                placeholder="Enter Username"
+                required
+              />
+              <br/>
+            </div>
+            <div className="text-left padding-l-20x padding-r-20x">
+              <label>Password:</label>
+              <br/>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                required
+                className="border-b-2 border-gray-200 w-[100%]"
+              />
+              <br/>
+            </div>
+            <br/>
+            <button type="submit">Login</button>
+            <label>
+              <br/>
+              <input type="checkbox" /> Remember me
+            </label>
+            <span>
+           <a href="#">    Forgot password?</a>
+            </span>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
 
 /* <div>
 <label>Username/Gmail :</label>
@@ -53,14 +53,5 @@ export default LoginPage
 </div><div><label>Password:</label><br/>
 <input type="text"/>
 </div> */
-
-
-
-
-
-
-
-
-
 
 /** */
